7b759a74994bc3591f36bde9d39de1c1a7e9e4f7
valen094 <ramirezvalentina783@gmail.com>
2024-12-21 01:57:30 -0500

    scroll en registro

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-import {getUsers,saveUsers,getUser,getIndex} from'./localStorage.js'
-import {User} from './user.js'
+import { getUsers, saveUsers, getUser, getIndex } from './localStorage.js'
+import { User } from './user.js'
 
-let btn=document.getElementById("send")
+let btn = document.getElementById("send")
 
 // validamos si el correo y la contraseña son correctos para iniciar sesion y si lo son creamos un token 
-export function login(){
-    let users=getUsers()
-    let email=document.getElementById("email").value
-    let password=document.getElementById("password").value
-    let user= users.filter(user=> user.email ==email && user.password==password)
+export function login() {
+    let users = getUsers()
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let user = users.filter(user => user.email == email && user.password == password)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular para validar correos electrónicos
 
     if (!emailRegex.test(email)) {
@@ -18,45 +18,58 @@ export function login(){
             icon: "warning",
             timer: 3000, // Tiempo en milisegundos (3 segundos)
             showConfirmButton: false, // Oculta el botón de confirmación
-          });
+        });
         return; // Detenemos la ejecución si el correo no es válido
     }
-    if(password.length<1){
+    if (password.length < 1) {
         Swal.fire({
             title: "¡Oh, Oh!",
             text: "¡ingresa una contraseña!",
             icon: "warning",
             timer: 4000, // Tiempo en milisegundos (3 segundos)
             showConfirmButton: false, // Oculta el botón de confirmación
-          });
-          return;
+        });
+        return;
     }
-    if(user.length==0){
+    if (user.length == 0) {
         Swal.fire({
             title: "¡Oh, Oh!",
             text: "Contraseña o correo incorrecto!",
             icon: "error",
             timer: 4000, // Tiempo en milisegundos (3 segundos)
             showConfirmButton: false, // Oculta el botón de confirmación
-          });
-          return;
+        });
+        return;
     }
-    
+
     localStorage.removeItem('token')
-    localStorage.setItem('token',user[0].id)//+Math.random().toString(36).substr(2)
+    localStorage.setItem('token', user[0].id)//+Math.random().toString(36).substr(2)
     Swal.fire({
         title: "¡Bienvenido!",
         text: "Disfruta de todas las herramientas que te ofrecemos!",
         icon: "success",
         timer: 4000, // Tiempo en milisegundos (3 segundos)
         showConfirmButton: false, // Oculta el botón de confirmación
-        });
-        setTimeout(()=>{window.location.href = "/index.html";},4000)
+    });
+    setTimeout(() => { window.location.href = "/index.html"; }, 4000)
 
-   
+
 }
 // login()
-btn.addEventListener('click',()=>{login()})
+btn.addEventListener('click', () => { login() })
+
+
+document.getElementById('scrollToRegister').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const targetUrl = new URL(this.href);
+    const sectionId = targetUrl.hash.substring(1);
+
+    localStorage.setItem('scrollToSection', sectionId);
+
+    window.location.href = targetUrl.origin + targetUrl.pathname;
+});
+
 
 
 
